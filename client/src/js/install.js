bed0190eee5ca6f8dc454d7c19af4dafd4160f2e
Jonathan Maldonado Guemarez <cantor.dedekind@gmail.com>
2023-11-13 12:38:02 -0500

    Small change to index.html and some refactoring of install.js, changing local(module level) variable for global one and setting the hidden attribute differently.

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,16 +1,15 @@
 const butInstall = document.getElementById('buttonInstall');
-let installPrompt = null;
 // Logic for installing the PWA
 window.addEventListener("beforeinstallprompt", (event) => {
     event.preventDefault();
     // Store a reference within a global variable to the event triggered on load for later use
-    installPrompt = event;
+    window.deferredPrompt = event;
     //Removing the hidden attribute from the install button.
-    butInstall.removeAttribute('hidden');
+    butInstall.hidden = false;
   });
 
 butInstall.addEventListener("click", async () => {
-    const promptEvent = installPrompt;
+    const promptEvent = window.deferredPrompt;
     if (!promptEvent) {
       return;
     }
@@ -20,58 +19,12 @@ butInstall.addEventListener("click", async () => {
 
     console.info(`Install prompt was: ${result.outcome}`);
     // Reset the variable,hence avoiding repeated installation prompts.
-    installPrompt = null;
+    window.deferredPrompt = null;
     //Adding the hidden attribute to the install button once the app is installed on the user's local disk.
-    butInstall.setAttribute('hidden', "");
+    butInstall.hidden = true;
   });
 
 window.addEventListener("appinstalled", () => {
-    console.info('JATE app is already installed on your system!');
     // Reset the variable,hence avoiding repeated installation prompts once the app is installed.
-    installPrompt = null;
-});
-/*
-const butInstall = document.getElementById('buttonInstall');
-
-// Logic for installing the PWA
-window.addEventListener('beforeinstallprompt', (event) => {
-  // Prevent Chrome 67 and earlier from automatically showing the prompt
-  event.preventDefault();
-  // Stash the event so it can be triggered later.
-  window.deferredPrompt = event;
-  // Update UI notify the user they can install the PWA
-  butInstall.style.display = 'block';
-});
-
-butInstall.addEventListener('click', () => {
-  // Show the prompt if deferredPrompt is stored
-  if (window.deferredPrompt) {
-    window.deferredPrompt.prompt();
-    window.deferredPrompt.userChoice.then((choiceResult) => {
-      if (choiceResult.outcome === 'accepted') {
-        console.log('User accepted the A2HS prompt');
-      } else {
-        console.log('User dismissed the A2HS prompt');
-      }
-      window.deferredPrompt = null;
-      butInstall.style.display = 'none';
-    });
-  } else {
-    // The user hit the install button but the app can't prompt to install (likely already installed)
-    alert('The app is already installed or not available for install.');
-  }
-});
-
-window.addEventListener('appinstalled', (event) => {
-  // Log the installation to analytics or console
-  console.log('PWA was installed', event);
-  // Hide the install button after the app is installed
-  butInstall.style.display = 'none';
-  // Clear any stored deferredPrompt since the app was installed
   window.deferredPrompt = null;
 });
-
-// On load, or when the 'load' event fires, check if the PWA is already installed.
-window.addEventListener('load', () => {
-  butInstall.style.display = 'block'; // Always show the install button until app is installed
-});*/
