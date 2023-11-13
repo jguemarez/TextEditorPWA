ac987b9a35e0d2deed399cd69c0999fac4b65f5a
Jonathan Maldonado Guemarez <cantor.dedekind@gmail.com>
2023-11-13 09:41:36 -0500

    Added code in install.js in order to add and remove the hidden attribute from the install button. Modified the in the index.js file in order to see if the load spinner finally shows.

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -4,6 +4,8 @@ const butInstall = document.getElementById('buttonInstall');
 window.addEventListener("beforeinstallprompt", (event) => {
     // Store a reference within a global variable to the event triggered on load for later use
     window.deferredPrompt = event;
+    //Removing the hidden attribute from the install button.
+    butInstall.removeAttribute('hidden');
   });
 
 butInstall.addEventListener("click", async () => {
@@ -14,12 +16,18 @@ butInstall.addEventListener("click", async () => {
   
     // Show the install prompt, so the web app can used as a native application.
     promptEvent.prompt();
+
+    const result = await promptEvent.userChoice;
+    console.log(result);
   
     // Reset the variable,hence avoiding repeated installation prompts.
     window.deferredPrompt = null;
+    //Adding the hidden attribute to the install button once the app is installed on the user's local disk.
+    butInstall.setAttribute('hidden', true);
   });
 
-window.addEventListener("appinstalled", (event) => {
+window.addEventListener("appinstalled", () => {
+    console.info('JATE app is already installed on your system!');
     // Reset the variable,hence avoiding repeated installation prompts once the app is installed.
     window.deferredPrompt = null;
 });
